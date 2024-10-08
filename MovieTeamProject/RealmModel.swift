37e8ff61e907921eaf453ae0697567be3463700c
Joy <kimjoydev@gmail.com>
2024-10-08 18:39:38 +0900

    Init: Realm 파일 생성

---
+++ MovieTeamProject/RealmModel.swift
@@ -0,0 +1,14 @@
+//
+//  RealmModel.swift
+//  MovieTeamProject
+//
+//  Created by Joy Kim on 10/8/24.
+//
+
+import Foundation
+import RealmSwift
+
+
+class Media: Object, ObjectKeyIdentifiable {
+   
+    }
